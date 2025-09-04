cfc6312cd804dcb2fa81aaee5bc0e7d43a74fb45
Sam Liu <sam@samliu.dev>
2025-09-04 14:46:34 -0400

    Use solid_cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
-  # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" } }
+  # Use Solid Cache for database-backed caching
+  config.cache_store = :solid_cache_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
