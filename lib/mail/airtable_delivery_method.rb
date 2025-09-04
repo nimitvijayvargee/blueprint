3785a22623854eb8b8c3650234db1e610b061140
Sam Liu <sam@samliu.dev>
2025-09-04 14:31:32 -0400

    Use mail namespace

--- lib/mail/airtable_delivery_method.rb
+++ lib/mail/airtable_delivery_method.rb
@@ -1,14 +1,16 @@
-class AirtableDeliveryMethod
-  def initialize(values)
-    # values is a hash of options from config.action_mailer.airtable_delivery_method_settings
-  end
+module Mail
+  class AirtableDeliveryMethod
+    def initialize(values)
+      # values is a hash of options from config.action_mailer.airtable_delivery_method_settings
+    end
 
-  def deliver!(mail)
-    Faraday.post(ENV["EMAIL_AIRTABLE_WEBHOOK"], {
-      token: ENV["EMAIL_TOKEN"],
-      email: mail.to,
-      subject: mail.subject,
-      body: mail.body.to_s
-    }.to_json, { "Content-Type" => "application/json" })
+    def deliver!(mail)
+      Faraday.post(ENV["EMAIL_AIRTABLE_WEBHOOK"], {
+        token: ENV["EMAIL_TOKEN"],
+        email: mail.to,
+        subject: mail.subject,
+        body: mail.body.to_s
+      }.to_json, { "Content-Type" => "application/json" })
+    end
   end
 end
