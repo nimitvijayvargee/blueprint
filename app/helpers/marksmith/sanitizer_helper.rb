c38c353fad55ffc84493aa20edbb70c01a137622
Sam Liu <sam@samliu.dev>
2025-09-19 11:19:39 -0400

    Disable external images

--- app/helpers/marksmith/sanitizer_helper.rb
+++ app/helpers/marksmith/sanitizer_helper.rb
@@ -4,35 +4,55 @@ module Marksmith
       sanitized = sanitize(html.to_s, tags: allowed_marksmith_tags, attributes: allowed_marksmith_attributes)
 
       doc = Nokogiri::HTML::DocumentFragment.parse(sanitized)
-      doc.css('a[href]').each do |a|
-        # Always overwrite rel on all anchors to prevent user-injected values
-        a['rel'] = 'nofollow noopener'
 
-        href = a['href'].to_s
+      # Replace external images with a callout linking to the original URL
+      doc.css("img[src]").each do |img|
+        src = img["src"].to_s
+        next unless external_link?(src)
+
+        wrapper = Nokogiri::XML::Node.new("div", doc)
+        wrapper["class"] = "external-image-callout"
+        wrapper.add_child(Nokogiri::XML::Text.new("Displaying external images is not supported. ", doc))
+
+        link = Nokogiri::XML::Node.new("a", doc)
+        link["href"] = src
+        link["rel"] = "nofollow noopener"
+        link["target"] = "_blank"
+        link.content = "See original"
+
+        wrapper.add_child(link)
+        img.replace(wrapper)
+      end
+
+      # Normalize anchors: enforce rel, add target for external when missing
+      doc.css("a[href]").each do |a|
+        a["rel"] = "nofollow noopener"
+
+        href = a["href"].to_s
         if external_link?(href)
-          # Only add target when not provided; do not override existing target
-          a['target'] = a['target'].presence || '_blank'
+          a["target"] = a["target"].presence || "_blank"
         end
       end
+
       doc.to_html.html_safe
     end
 
     private
 
     def allowed_marksmith_tags
-      ( %w[table th tr td span em strong i] +
+      (%w[table th tr td span em strong i] +
         ActionView::Helpers::SanitizeHelper.sanitizer_vendor.safe_list_sanitizer.allowed_tags.to_a
       ).uniq
     end
 
     def allowed_marksmith_attributes
-      ( %w[href rel target src alt title width height class] +
+      (%w[href rel target src alt title width height class] +
         ActionView::Helpers::SanitizeHelper.sanitizer_vendor.safe_list_sanitizer.allowed_attributes.to_a
       ).uniq
     end
 
     def external_link?(href)
-      return false if href.start_with?('#', '/', './', '../')
+      return false if href.start_with?("#", "/", "./", "../")
       return false unless href =~ /\Ahttps?:\/\//i
 
       base_url = request.base_url rescue nil
@@ -48,7 +68,7 @@ module Marksmith
     end
 
     def default_port(scheme)
-      scheme.to_s.downcase == 'https' ? 443 : 80
+      scheme.to_s.downcase == "https" ? 443 : 80
     end
   end
 end
