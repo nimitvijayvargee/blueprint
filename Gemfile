ebaa48ce492007a336ed0110cb5a9109c55f39b3
Sam Liu <sam@samliu.dev>
2025-09-19 13:53:31 -0400

    Add R2 for development

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
 
+gem "aws-sdk-s3", require: false
+
 gem "tailwindcss-rails", "~> 4.0"
 
 # Redis for cache, queue, and Action Cable
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.cache_store = :memory_store
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudflare
 
 
   # Use custom Airtable delivery method for mailer
