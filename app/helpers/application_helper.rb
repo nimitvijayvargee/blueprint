1096596470d6ab9814a9b75704986ed111fb1b6f
Sam Liu <sam@samliu.dev>
2025-09-22 11:56:14 -0400

    Update admin dash styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def admin_namespace?
+    controller_path.start_with?("admin/")
+  end
+
   # Shorthand helper for merging Tailwind class strings in views.
   # Accepts strings/arrays/nil values, just like Tailwind.merge.
   def tw(*classes)
