03f6447ed96fde8d47e0dc29529fb73386c95d49
Sam Liu <sam@samliu.dev>
2025-10-03 13:05:12 -0400

    Hide delete projects in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @projects = current_user.projects.includes(:banner_attachment)
+    @projects = current_user.projects.where(is_deleted: false).includes(:banner_attachment)
   end
 end
