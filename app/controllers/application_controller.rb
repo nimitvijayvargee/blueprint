a228139091c141117ec2e3976e4bdaf2e73de5ad
Sam Liu <sam@samliu.dev>
2025-10-03 12:48:37 -0400

    Track utm source

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,23 @@ class ApplicationController < ActionController::Base
   include Authentication
   include SentryContext
   include Pagy::Backend
+  
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   before_action :set_paper_trail_whodunnit
   before_action :update_last_active
+  before_action :track_ahoy_visit
 
   def not_found
     raise ActionController::RoutingError.new("Not Found")
   end
 
   private
+
+  def track_ahoy_visit
+    ahoy.track_visit
+  end
 
   def update_last_active
     return unless current_user
