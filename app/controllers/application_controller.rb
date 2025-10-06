e809737c8d348dc47b7ffe386dca3620c28b0332
Sam Liu <sam@samliu.dev>
2025-10-06 09:29:39 -0400

    Allow non-modern browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 
   before_action :set_paper_trail_whodunnit
   before_action :update_last_active
