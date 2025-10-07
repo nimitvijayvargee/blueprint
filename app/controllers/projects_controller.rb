6af08f1c86d3f6a6698dae5db4c84e7d52ebaec7
Sam Liu <sam@samliu.dev>
2025-10-07 18:21:08 -0400

    Update minimum journal entry requirements

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -29,14 +29,14 @@ class ProjectsController < ApplicationController
 
     repo_linked = @project.repo_link.present?
     desc_ok = @project.description.to_s.strip.length >= 50
-    journal_ok = @project.journal_entries.exists?
+    journal_ok = @project.journal_entries.count >= 3
 
     @checks = [
       { msg: "GitHub repo linked", met: repo_linked },
       { key: "bom", msg: "Bill of materials (bom.csv) present", met: nil },
       { key: "readme", msg: "README.md present", met: nil },
       { msg: "Description is at least 50 characters", met: desc_ok },
-      { msg: "Project has journal entries", met: journal_ok }
+      { msg: "Project has 3 journal entries", met: journal_ok }
     ]
 
     @base_ok = repo_linked && desc_ok && journal_ok
