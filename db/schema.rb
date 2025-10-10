6c90bda4a4e5daedbeda3e5f4f7e79891c9364f2
Sam Liu <sam@samliu.dev>
2025-10-10 13:34:45 -0400

    Fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -480,7 +480,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_09_210827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", null: false
-    t.boolean "is_mcg", default: true, null: false
+    t.boolean "is_mcg", default: false, null: false
     t.string "github_username"
     t.datetime "last_active"
     t.bigint "github_installation_id"
