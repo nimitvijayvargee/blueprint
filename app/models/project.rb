73fbe77d531beb65ec7d806f1fa77718649bc665
Sam Liu <sam@samliu.dev>
2025-10-15 16:50:47 -0400

    Fix airtable sync

--- app/models/project.rb
+++ app/models/project.rb
@@ -562,7 +562,7 @@ class Project < ApplicationRecord
       "Grant Amount" => grant ? (grant / 100.0) : nil,
       "Grant Tier" => tier,
       "Hours Self-Reported" => journal_entries.sum(:duration_seconds) / 3600.0,
-      "Project Record" => id
+      "BP Project" => id
     }
 
     AirtableSync.upload_or_create!(
